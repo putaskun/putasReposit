91e3e10775b48779a143c6e4030ac2c1c8fcda54
putaskun <buta.rakuda@gmail.com>
2016-04-25 22:36:56 +0900

    Added files via upload

--- index.php
+++ index.php
@@ -1,2 +1,485 @@
-<?php
-// Silence is golden.
+ <?php get_header(); ?>
+
+
+
+<!--balloon_javascript-->
+<script>$(function() {
+$('div.coachtext a').balloon();
+});
+</script>
+<!--/balloon_javascript-->
+
+
+
+<ul class="cb-slideshow">
+		<li><span class="spImg">Image 01</span><div><h3>チアで世界に通用する力を手に入れる！<br><span class="spFont">GLOBAL CHEER LEEDING</span></h3></div></li>
+		<li><span class="spImg">Image 02</span><div><h3>チアで世界に通用する力を手に入れる！<br><span class="spFont">GLOBAL CHEER LEEDING</span></h3></div></li>
+		<li><span class="spImg">Image 03</span><div><h3>チアで世界に通用する力を手に入れる！<br><span class="spFont">GLOBAL CHEER LEEDING</span></h3></div></li>
+</ul>
+
+
+
+<div class="wrapper">
+
+<!--about-->
+		<div class="about">
+			<h2><p><img src="<?php echo get_template_directory_uri(); ?>/images/pom_green.png"><p>グローバルチアについて<br><span>ABOUT</span><p></h2>
+			<ul class="clearfix">
+				<li><span class="kiso"><br>基礎体力</span></li>
+				<li><span class="hyogen">2<br>表現力</span></li>
+				<li><span class="houshi">3<br>奉仕の精神</span></li>
+			</ul>
+
+			<p>Starssmileyのチアダンススクールは、世界で通用する力を手に入れるプログラムを用意しています！
+				英語を活用したダンス指導のほか、基礎体力・表現・奉仕の精神を柱に、踊っている自分自身も、
+				見ている人も笑顔で元気にするダンスを目指しています！
+			</p>
+		</div>
+<!--/about-->
+
+<!--testdayou-->
+<div class="tsue"></div>
+<!--testdayou-->
+
+<!--about7-->
+		<div class="about7">
+			<h2><p><img src="<?php echo get_template_directory_uri(); ?>/images/pom_cream.png"></p><p>グローバルチアで身につく7つのこと<br><span>ABOUT</span></p></h2>
+			<p class="about7P">外見だけでなく、内面も磨いて、世界で通用する女性を目指しましょう！</p>
+			<ul>
+				<li class="clearfix">
+            <div class="reigi">
+                <span class="oya"><span class="textbig">1</span>礼儀</span>
+                <div class="imgpom"></div>
+                <p class="circlephoto"><img src="<?php echo get_template_directory_uri(); ?>/images/about7_1.png"></p>
+            </div>
+            <p class="ptext">元気な挨拶・感謝の気持ちを言葉で伝える、先生の話をよく聞く</p>
+        </li>
+        <li class="clearfix">
+            <div class="sekkyokusei">
+                <span class="oya"><span class="textbig">2</span>積極性</span>
+                <div class="imgpom"></div>
+                <p class="circlephoto"><img src="<?php echo get_template_directory_uri(); ?>/images/about7_2.png"></p>
+            </div>
+            <p class="ptext">笑顔で人前に立つ、ダンスを披露する、仲間同士で意見を言い合う</p>
+        </li>
+        <li class="clearfix">
+            <div class="kyouchosei">
+                <span class="oya"><span class="textbig">3</span>協調性</span>
+                <div class="imgpom"></div>
+                <p class="circlephoto"><img src="<?php echo get_template_directory_uri(); ?>/images/about7_3.png"></p>
+            </div>
+            <p class="ptext">ダンスを揃える、仲間と協力しあってダンスを作り上げる</p>
+        </li>
+        <li class="clearfix">
+            <div class="syucyuryoku">
+                <span class="oya"><span class="textbig">4</span>集中力</span>
+                <div class="imgpom"></div>
+                <p class="circlephoto"><img src="<?php echo get_template_directory_uri(); ?>/images/about7_4.png"></p>
+            </div>
+            <p class="ptext">１時間のレッスンを集中して受ける、２分半のダンスを覚える</p>
+        </li>
+        <li class="clearfix">
+            <div class="hyogenryoku">
+                <span class="oya"><span class="textbig">5</span>表現力<br><span class="presnttext">(プレゼンテーション力)</span></span>
+                <div class="imgpom"></div>
+                <p class="circlephoto"><img src="<?php echo get_template_directory_uri(); ?>/images/about7_5.png"></p>
+            </div>
+            <p class="ptext">目標にむかって努力する、評価を受け止めてステップアップしていく</p>
+        </li>
+        <li class="clearfix">
+            <div class="taseikan">
+                <span class="oya"><span class="textbig">6</span>達成感</span>
+                <div class="imgpom"></div>
+                <p class="circlephoto"><img src="<?php echo get_template_directory_uri(); ?>/images/about7_6.png"></p>
+            </div>
+            <p class="ptext">柔軟性、筋力、持久力がつき、健康的な身体になる</p>
+        </li>
+        <li class="clearfix">
+            <div class="volunteer taseikan">
+                <span class="oya"><span class="textbig">7</span>ボランティア精神</span>
+                <div class="imgpom"></div>
+                <p class="circlephoto"><img src="<?php echo get_template_directory_uri(); ?>/images/about7_7.png"></p>
+            </div>
+            <p class="ptext">チームメイトを思いやり、助け合う気持ちが育ち、チア以外の生活においてもボランティア精神を身につけることができる(年に数回、老人ホーム慰問実施
+              )</p>
+        </li>
+
+        <li class="descript"><span>
+          Starssmileyのチアダンススクールは、世界で通用する力を手に入れるプログラムを用意しています！英語を活用したダンス指導のほか、基礎体力・表現・奉仕の精神を柱に、踊っている自分自身も、
+    				見ている人も笑顔で元気にするダンスを目指しています！
+          </span>
+        </li>
+
+      </ul>
+
+		</div>
+<!--/about7-->
+
+
+
+
+<!--class-->
+		<div class="classu">
+			<h2><p><img src="<?php echo get_template_directory_uri(); ?>/images/pom_pink.png"></p><p class="subtitle">クラスのご紹介<br><span>CLASS</span></p></h2>
+
+			<p>ベビーからキッズまで、月齢にあわせたプログラムで指導していきます！</p>
+
+
+			<ul>
+				<li><p>ベビーダンス<span class="classlogo mama"><img src="<?php echo get_template_directory_uri(); ?>/images/classlogo1.png"></span>首据わり～2歳頃までのベビー＆ママ（パパ）</p><p><img src="<?php echo get_template_directory_uri(); ?>/images/classphoto1.png"></p><p>ベビーを抱っこしながらダンスステップを踏むことで、お子様のリズム感を養いながら親子で運動する楽しさをお伝えします。簡単なベビーマッサージや絵本も紹介<br><i class="fa fa-star"></i>単発1,300円</p><p class="moreB">MORE</p></li>
+				<li><p>プレベビーチア<span class="classlogo"><img src="<?php echo get_template_directory_uri(); ?>/images/classlogo2.png"></span>1歳3ヶ月～2歳の親子</p><p><img src="<?php echo get_template_directory_uri(); ?>/images/classphoto1.png"></p><p>よちよち歩き始めた過渡期のベビーたちへ、公園遊び＆屋内遊びの両方を取り入れ、体幹を鍛える基礎となる動きをお伝えします。<br><i class="fa fa-star"></i>3か月ごとにレッスン生募集体験1,750円(体験レッスンは随時受付)</p><p class="moreB">MORE</p></li>
+			</ul>
+			<ul>
+				<li><p>ベビーチア<span class="classlogo baby"><img src="<?php echo get_template_directory_uri(); ?>/images/classlogo3.png"></span>1歳8ヶ月～3歳くらいまでの親子</p><p><img src="<?php echo get_template_directory_uri(); ?>/images/classphoto1.png"></p><p>小走りを始めたキッズ〜未就園児の親子体操。ご挨拶をする・順番を守るなどの社会性も大切にしながら、ダンス・鉄棒・跳び箱・マットなどの学校体育にチャレンジしていきます。<br><i class="fa fa-star"></i>体験1,600円</p><p class="moreB">MORE</p></li>
+				<li><p>キッズチアダンス<span class="classlogo"><img src="<?php echo get_template_directory_uri(); ?>/images/classlogo4.png"></span>3歳～小学校低学年</p><p><img src="<?php echo get_template_directory_uri(); ?>/images/classphoto1.png"></p><p>チアダンスの技術面だけでなく「グローバルに活躍できる人になること」を目標に、挨拶・礼儀など内面を輝かせることも重視したレッスン内容です。
+<br><i class="fa fa-star"></i><strong>毎年3月頃にレッスン生募集。定員になり次第締め切り。</strong></p><p class="moreB">MORE</p></li>
+			</ul>
+
+			<p>Starssmileyのチアダンススクールは、世界で通用する力を手に入れるプログラムを用意しています！
+				英語を活用したダンス指導のほか、基礎体力・表現・奉仕の精神を柱に、踊っている自分自身も、
+				見ている人も笑顔で元気にするダンスを目指しています！
+			</p>
+
+		</div>
+<!--/class-->
+
+
+
+
+<!--classTset-->
+		<div class="classu">
+			<h2><p><img src="<?php echo get_template_directory_uri(); ?>/images/pom_pink.png"></p><p class="subtitle">クラスのご紹介<br><span>CLASS</span></p></h2>
+
+			<p>ベビーからキッズまで、月齢にあわせたプログラムで指導していきます！</p>
+
+
+
+<ul>
+<li>
+  <p>ベビーダンス<span class="classlogo mama"><img src="<?php echo get_template_directory_uri(); ?>/images/classlogo1.png"></span>首据わり～2歳頃までのベビー＆ママ（パパ）
+  </p>
+      <?php
+      $args = array(
+           'post_type' => 'post', // 投稿タイプを指定
+           //'posts_per_page' => 2, // 表示するページ数
+          'showposts'=> '1',
+          'orderby'  => 'date',
+          'category_name'=> "babydance"
+      ); ?>
+      <?php $wp_query = new WP_Query( $args ); ?><!-- クエリの指定 -->
+      <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+      <!-- ここに表示するタイトルやコンテンツなどを指定 -->
+      <div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail() ?> </a></div>
+      <p><?php the_content('MORE &raquo;');?></p>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?><!-- 忘れずにリセットする必要がある -->
+</li>
+<!--/classTest-->
+
+<li>
+  <p>プレベビーチア<span class="classlogo"><img src="<?php echo get_template_directory_uri(); ?>/images/classlogo2.png"></span>1歳3ヶ月～2歳の親子
+  </p>
+<!--classTset-->
+      <?php
+      $args = array(
+           'post_type' => 'post', // 投稿タイプを指定
+           //'posts_per_page' => 2, // 表示するページ数
+          'showposts'=> '1',
+          'orderby'  => 'date',
+          'showposts'=> 'prebaby',
+          'category_name'=> ""
+      ); ?>
+      <?php $wp_query = new WP_Query( $args ); ?><!-- クエリの指定 -->
+      <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+      <!-- ここに表示するタイトルやコンテンツなどを指定 -->
+      <div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail() ?> </a></div>
+      <p><?php the_content('MORE &raquo;');?></p>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?><!-- 忘れずにリセットする必要がある -->
+
+<!--/classTest-->
+</li>
+</ul>
+
+<ul>
+<li>
+  <p>ベビーチア<span class="classlogo baby"><img src="<?php echo get_template_directory_uri(); ?>/images/classlogo3.png"></span>1歳8ヶ月～3歳くらいまでの親子
+  </p>
+<!--classTset-->
+      <?php
+      $args = array(
+           'post_type' => 'post', // 投稿タイプを指定
+           //'posts_per_page' => 2, // 表示するページ数
+          'showposts'=> '1',
+          'orderby'  => 'date',
+          'category_name'=> "babycheer"
+      ); ?>
+      <?php $wp_query = new WP_Query( $args ); ?><!-- クエリの指定 -->
+      <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+      <!-- ここに表示するタイトルやコンテンツなどを指定 -->
+      <div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail() ?> </a></div>
+      <p><?php the_content('MORE &raquo;');?></p>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?><!-- 忘れずにリセットする必要がある -->
+
+<!--/classTest-->
+</li>
+
+<li>
+  <p>キッズチアダンス<span class="classlogo"><img src="<?php echo get_template_directory_uri(); ?>/images/classlogo4.png"></span>3歳～小学校低学年
+  </p>
+<!--classTset-->
+      <?php
+      $args = array(
+           'post_type' => 'post', // 投稿タイプを指定
+           //'posts_per_page' => 2, // 表示するページ数
+          'showposts'=> '1',
+          'orderby'  => 'date',
+          'category_name'=> "kidscheer"
+      ); ?>
+      <?php $wp_query = new WP_Query( $args ); ?><!-- クエリの指定 -->
+      <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+      <!-- ここに表示するタイトルやコンテンツなどを指定 -->
+      <div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail() ?> </a></div>
+      <p><?php the_content('MORE &raquo;');?></p>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?><!-- 忘れずにリセットする必要がある -->
+</li>
+</ul>
+
+</div>
+<!--/class-->
+
+
+<!--campaign-->
+		<div class="campaign">
+			<h2><p><img src="<?php echo get_template_directory_uri(); ?>/images/pom_cream.png"><p>体験・キャンペーンのご案内<p></h2>
+
+
+			<ul>
+				<li><p><img src="<?php echo get_template_directory_uri(); ?>/images/campaign.png"></p><p>チアダンスは毎年2月・3月頃に体験会を行っております。それ以外の月は、個別にお問合せ下さい。</p></li>
+				<li><p><img src="<?php echo get_template_directory_uri(); ?>/images/campaign.png"></p><p>ベビーダンス、プレ・ベビーチア、ベビーチア、
+この他のレッスンについては、随時体験受付中です。</p></li>
+				<li><p><img src="<?php echo get_template_directory_uri(); ?>/images/campaign.png"></p><p>季節にあわせたイベントも行っております。
+詳しくはスケジュール表をご覧下さい。</p></li>
+			</ul>
+		</div>
+<!--/campaign-->
+
+<!--voice-->
+		<div class="voice">
+			<h2><p><img src="<?php echo get_template_directory_uri(); ?>/images/pom_green.png"><p>お客様の声<br><span>VOICE</span><p></h2>
+
+			<ul>
+				<li>
+          <span class="comme"><img src="<?php echo get_template_directory_uri(); ?>/images/voicecomme.png"></span>
+          <p>
+            <span class="commeText">ベビーチアを通して積極的になりました！親子の思い出づくりにもぴったり♪</span>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/voice1.png">
+          </p>
+          <p class="commedscrpt">Fちゃん親子</p>
+          <p>2才からベビーチアを始め、最初の頃は私から離れなかったのですが、だんだん積極性がでてきて、自分から手を挙げて自己紹介をしたり、運動も上の学年の子と競うぐらいに成長しました。また、親子で参加できる発表会もあり、思い出作りもできます。先生や友達には、感謝しています。
+          </p>
+        </li>
+
+				<li>
+          <span class="comme"><img src="<?php echo get_template_directory_uri(); ?>/images/voicecomme.png"></span>
+          <p>
+            <span class="commeText">一度も休みたい、辞めたいと言うことはなく、とにかくチアを楽しんでいます！</span>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/voice2.png">
+          </p>
+          <p class="commedscrpt">Sちゃん親子</p>
+          <p>娘は年中の頃から、チアダンスを習いたいと言い続け、お友達の紹介で入会しました。3年目になり、1度も休みたい、辞めたいと言わずに、チアダンスを楽しんでいます。レッスンはメリハリがあり、自分なりの目標も出来ました。チアダンスで学ぶ事はたくさんあるので、これからも続けていきたいです。
+          </p>
+        </li>
+
+				<li>
+          <span class="comme"><img src="<?php echo get_template_directory_uri(); ?>/images/voicecomme.png"></span>
+          <p>
+            <span class="commeText">コーチのみなさんに、心の大切な部分を育てていただきました♪</span>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/voice3.png">
+          </p>
+          <p class="commedscrpt">Mくん親子</p>
+          <p>入会のきっかけは楽しそうだったからと単純なものです。受講してみると、小さな頃は見て真似る事を覚え、大きくなるにつれ小さな子を応援する優しい気持ち、手本になろうと頑張る気持ちが育ち心の大切な部分を育てて頂けたと思います。
+          </p>
+        </li>
+
+			</ul>
+		</div>
+<!--/voice-->
+
+<!--FAQ-->
+		<div class="faq">
+			<h2><p><img src="<?php echo get_template_directory_uri(); ?>/images/pom_yellow.png"><p>よくあるご質問<br><span>FAQ</span><p></h2>
+
+
+			<ul class="clearfix">
+				<li><div class="Q">今までチアダンスをしたことが無いのですが、入会できますか？</div><div class="Ans">リトルクラス(年少組)へは、ベビーチア卒業生から優先的に受付しております。3月以降に残席がある場合のみ、全クラス新規レッスン生を募集いたします。基礎的な運動能力・本人のやる気などを拝見させていただきますので、ぜひお問合せ下さい。</div></li>
+				<li><div class="Q">歩き始めの活発な子なのですが、プレ・ベビーチアとベビーチアのどちらのレッスンが合っていますか？</div><div class="Ans">同時期に両方の体験レッスンをご受講いただくことも可能です。お子様のタイプにもよりますが、
+ご心配な場合にはプレ・ベビーチアからご参加されることをお勧めしております。</div></li>
+				<li><div class="Q">チアというと女の子というイメージですが、男の子も参加可能ですか？</div><div class="Ans">「ベビーチア」は未就園児の親子体操レッスンとなっておりますので、
+男の子にも多数ご参加いただいております。また年少以降のチアダンスクラスにも男の子が在籍しておりますので、安心してご参加下さい。</div></li>
+			</ul>
+		</div>
+<!--/FAQ-->
+
+<!--STAFF-->
+		<div class="coach">
+			<h2><p><img src="<?php echo get_template_directory_uri(); ?>/images/pom_blue.png"><p>スタッフ<br><span>coach</span><p></h2>
+
+			<ul class="clearfix">
+				<li>
+          <div class="coachtext">
+            <a href="#" title="厳しい中にも<br>愛のある指導を<br>心がけています！">
+              <span class="murata">
+							      <span class="spnTop">代表・講師</span>
+                    <span class="spnUndr">村田&nbsp;綾子(Ayako&nbsp;Murata)</span>
+              </span>
+						</a>
+          </div>
+					<div class="Ans">日本女子体育大学卒業大学卒業後、テーマパークでシンガー・ダンサーで活躍。未就園児の親子体操「ベビーチア」発案。日本チアダンス協会インストラクター、USAチアダンス協会インストラクター3級、幼児体育協会インストラター、新浦安RAINBOW SMILEY、幕張インターナショナル、スクール「Dolphins」、たまプラーザStars Smiley、恵比寿Stars Smiley、デイレクター兼チーフインストラクター
+						現在、チアダンス・ベビーチア、幼児体操、障がい児体操、スポーツの家庭教師、インストラクターとして活動。
+					</div>
+				</li>
+
+				<li>
+					<div class="coachtext">
+            <a href="#" title="子どもの<br>やる気と自主性を<br>信じて待ちます！">
+              <span class="shikima">
+						          <span class="spnTop">講師</span>
+                      <span class="spnUndr">色摩&nbsp;由維(Yui&nbsp;Shikama)</span>
+              </span>
+					  </a>
+          </div>
+					<div class="Ans">東京家政学院短期大学　英語科卒業中学・高校バトン部、大学卒業後はミュージカル、芝居で活躍。
+新浦安RAINBOW SMILEYアシスタントインストラクター、幕張インターナショナル、スクール「dolphins」、恵比寿Stars Smiley、たまプラーザStars Smiley、チーフインストラクター
+現在、キッズチアダンス、ベビーチア、障がい児体操、スポーツの家庭教師インストラクターとして活動。
+					</div>
+				</li>
+
+				<li>
+					<div class="coachtext">
+            <a href="#" title="できた！うれしい！<br>こどもたちの気持ちを<br>大切に、元気いっぱい、<br>レッスンしましょう♪">
+              <span class="usuda">
+						          <span class="spnTop">講師</span>
+                      <span class="spnUndr">薄田&nbsp;昌子(Masako&nbsp;Usuda)</span>
+              </span>
+					  </a>
+          </div>
+					<div class="Ans">大阪芸術大学舞台芸術ミュージカルコース卒業中学・高校体操部で活躍。県大会種目別、床、入賞。
+その後、テーマパークでパフォーマーを経て、NSCA-CPT パーソナルトレーナーとして活動。たまプラーザStars Smiley、チーフインストラクター、恵比寿Stars Smiley、アシスタントインストラクター
+現在、キッズチアダンス、ベビーチア、幼児体操、障がい児体操、スポーツの家庭教師インストラクターとして活動。
+					</div>
+				</li>
+
+			</ul>
+
+		</div>
+<!--/STAFF-->
+
+<!--smileyHouse-->
+		<div class="smileyHouse">
+			<h2><p><img src="<?php echo get_template_directory_uri(); ?>/images/pom_yellow.png"><p>スマイリーハウスのご案内<br><span>FAQ</span><p></h2>
+
+
+			<ul class="clearfix">
+				<li>
+					<img src="<?php echo get_template_directory_uri(); ?>/images/smileyhouse.jpg" width="100%;">
+        </li>
+        <li class="textPdding">
+          <div class="textPdding">
+          <table>
+            <tr>
+              <th>住所</th>
+              <td>横浜市青葉区美しが丘5-6-1</td>
+            </tr>
+            <tr>
+              <th>最寄駅</th>
+              <td>田園都市線　たまプラーザ駅　徒歩５分</td>
+            </tr>
+            <tr>
+              <th>お問い合わせ</th>
+              <td>045-909-0081 (平日10:00-17:00)</td>
+            </tr>
+            <tr>
+              <th>メール</th>
+              <td>info@stars-smily.com</td>
+            </tr>
+            <tr>
+              <th>専用HP</th>
+              <td>http://ameblo.jp/stars-smiley</td>
+            </tr>
+          </table>
+        </div>
+        </li>
+      </ul>
+      <ul>
+				<li>
+          <div class="textPdding">
+            <h4>スマイリーハウスについて</h4>
+          横浜市青葉区美しが丘の住宅街に佇む一軒家の親子サロン。チアダンスをはじめとする各レッスンのほか、季節に合わせたイベントを行っています。世界の選りすぐりのおもちゃを毎月少しずつ買い揃え、お子様の学ぶ心も大切に。庭のある落ち着いたスタジオは、時間貸も承っておりますので随時お問合せ下さい。
+          </div>
+        </li>
+          <li>
+          <iframe src="http://localhost:8888/googlemap.html" width="100%"height="220px"frameborder="0" style="border:0" allowfullscreen></iframe>
+          </li>
+
+			</ul>
+
+      <!-- ここに表示するタイトルやコンテンツなどを指定 -->
+			<p class="moreB"><a href="<?php get_category_link( 13 ); ?>">MORE</a></p>
+		</div>
+<!--/smileyHouse-->
+
+<!--NEWS SNS-->
+		<div class="news">
+			<h2><p><img src="<?php echo get_template_directory_uri(); ?>/images/pom_cream.png"><p>最新情報・会社概要・採用・SNS・プライバシーポリシー<br><span>NEWS</span><p></h2>
+
+        <ul class="flex clearfix bg">
+
+
+
+
+        	</ul>
+
+
+
+        <ul class="flex clearfix bg">
+          <?php
+          $newsan = array(
+               'post_type' => 'post', // 投稿タイプを指定
+               //'posts_per_page' => 2, // 表示するページ数
+
+               'showposts'=> '3',
+               'orderby'  => 'date'
+          ); ?>
+          <?php $wp_query = new WP_Query( $newsan ); ?><!-- クエリの指定 -->
+          <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+          <!-- ここに表示するタイトルやコンテンツなどを指定 -->
+          <li>
+            <p><a href="<?php the_permalink() ?>"><?php the_title(); ?><?php the_category('') ?><?php the_post_thumbnail() ?> </a></p>
+            <p><?php if(mb_strlen($post->post_content)>38) { $content= mb_substr($post->post_content,0,38) ; echo $content. ･･･ ;
+} else {echo $post->post_content;}?></p>
+          </li>
+          <?php endwhile; ?>
+          <?php wp_reset_postdata(); ?><!-- 忘れずにリセットする必要がある -->
+
+        			</div>
+
+
+        </div>
+
+
+
+
+
+		</div>
+<!--/NEWS SNS-->
+
+
+	<!-- / warpper-->
+
+<div>
+
+</body>
+</html>

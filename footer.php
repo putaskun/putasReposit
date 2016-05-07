8d4a6d5aa11b3abd2a1c16b98c6c39aadc08a251
putaskun <buta.rakuda@gmail.com>
2016-05-07 13:05:53 +0900

    Added files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+  <!--FOOTER-->
+<footer>
+  <nav class="wrapper">
+    <ul>
+      <li><a href="">会社概要</a></li>
+      <li><a href="">個人情報・プライバシーポリシー</a></li>
+    </ul>
+    <p>© 2016 STARS SMILEY.</p>
+  </nav>
+
+</footer>
+  <!--/FOOTER-->

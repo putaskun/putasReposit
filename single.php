5860dbdcd57862e84f25aaa14f2a4c27627cef8a
putaskun <buta.rakuda@gmail.com>
2016-05-07 13:05:41 +0900

    Added files via upload

--- single.php
+++ single.php
@@ -28,12 +28,11 @@
             if( $prevpost or $nextpost ){ //前の記事、次の記事いずれか存在しているとき
             ?>
             <?php
-             if ( $prevpost ) { //前の記事が存在しているとき
-              echo '<a href="' . get_permalink($prevpost->ID) .'" id="prevli" class="clearfix">
-                    <div id="prev_title">«前の記事へ</div><ul class="prevnext clearfix">
-                    <li class="pagi-thumbox">' . get_the_post_thumbnail($prevpost->ID, array(100,100)) . '</li>
-                    <li>' . get_the_title($prevpost->ID) . '</li></ul>
-              </a>';
+            if ( $prevpost ) { //次の記事が存在しているとき
+             echo '<a href="' . get_permalink($prevpost->ID) .'" id="prevli" class="clearfix">
+                   <div id="prev_title">«前の記事へ</div><ul class="prevnext clearfix">
+               <li class="pagi-thumbox">' . get_the_post_thumbnail($prevpost->ID, array(100,100)).'</li>
+               <li class="pagi-title">'. get_the_title($prevpost->ID) . '</li></ul></a>';
              } else { //前の記事が存在しないとき
              echo  '<div id="prev_no"><a href="' .home_url('/'). '"><div id="prev_next_home"><i class="fa fa-home"></i>
               </div></a></div>';
@@ -42,7 +41,7 @@
               echo '<a href="' . get_permalink($nextpost->ID) .'" id="nextli" class="clearfix">
                     <div id="next_title">次の記事へ»</div><ul class="prevnext clearfix">
                 <li class="pagi-thumbox">' . get_the_post_thumbnail($nextpost->ID, array(100,100)).'</li>
-            		<li>'. get_the_title($nextpost->ID) . '</li></ul></a>';
+            		<li class="pagi-title">'. get_the_title($nextpost->ID) . '</li></ul></a>';
              } else { //次の記事が存在しないとき
              echo '<div id="next_no"><a href="' .home_url('/'). '"><div id="prev_next_home"><i class="fa fa-home"></i>
              </div></a></div>';
